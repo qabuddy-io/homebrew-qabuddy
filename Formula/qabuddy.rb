b1d6e9963087413d4d5e625ea07c8bd3e8050870
Luigi Donadel <luigi.donadel@gmail.com>
2024-11-06 20:09:31 +0100

    Update qabuddy.rb

--- Formula/qabuddy.rb
+++ Formula/qabuddy.rb
@@ -1,10 +1,10 @@
 class Qabuddy < Formula
   desc "CLI tool to automate the tests of your apps through QABuddy.io"
   homepage "https://github.com/qabuddy-io/homebrew-qabuddy"
-  url "https://github.com/qabuddy-io/homebrew-qabuddy/releases/download/1.1.5/qabuddy.tar.gz"
-  sha256 "b88460e4ac619e645ebcf1585981bfec6e04e2e29a1ad3e8f7998022c93cf9b5"
+  url "https://github.com/qabuddy-io/homebrew-qabuddy/releases/download/1.1.6/qabuddy.tar.gz"
+  sha256 "9fa62783326aaf6453385670618f1253537e287c2ecd0c976be7be659239b52e"
   license "MIT"
-  version "1.1.5"
+  version "1.1.6"
 
   depends_on "pipx"
   depends_on "cliclick"
