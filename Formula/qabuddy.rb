98867e4fee4d1ef48e0e32507dbd0b36036d85ff
Luigi Donadel <luigi.donadel@gmail.com>
2025-08-31 14:44:27 +0200

    Update SHA256 checksum for Qabuddy formula

--- Formula/qabuddy.rb
+++ Formula/qabuddy.rb
@@ -2,7 +2,7 @@ class Qabuddy < Formula
   desc "CLI tool to automate the tests of your apps through QABuddy.io"
   homepage "https://github.com/qabuddy-io/homebrew-qabuddy"
   url "https://github.com/qabuddy-io/homebrew-qabuddy/releases/download/1.2.0/qabuddy.tar.gz"
-  sha256 "fc1be00bdd0fb14d6c83fd2d99e098f6182887b17878eb995133bbd7f066c7d0"
+  sha256 "d9da47b4cf1922d2c5fb720f3c4f1488f33f916819f82f30d178b30897e83360"
   license "MIT"
   version "1.2.0"
 
